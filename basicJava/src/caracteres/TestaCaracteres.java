536e7cb03413d5a9df814498c5fbf2290015745f
Alexandre Junior <alexandrejunior408@gmail.com>
2023-08-28 13:35:38 -0300

    chore: add package to show testing on character variables

---
+++ basicJava/src/caracteres/TestaCaracteres.java
@@ -0,0 +1,23 @@
+package caracteres;
+
+public class TestaCaracteres {
+
+	public static void main(String[] args) {
+		char letra = 'a';
+		System.out.println(letra);
+		
+		char valor = 66;
+		System.out.println(valor);
+		
+		valor = (char) (valor + 1);
+		System.out.println(valor);
+		
+		String mensagem = "Eu estou aprendendo java ";
+		System.out.println(mensagem);
+		
+		mensagem = mensagem + "com Mateus";
+		System.out.println(mensagem);
+		
+	}
+
+}
---
+++ basicJava/src/caracteres/TestaVariaveis.java
@@ -0,0 +1,13 @@
+package caracteres;
+
+public class TestaVariaveis {
+	public static void main(String[] args) {
+		int primeiro = 5;
+		int segundo = 7;
+		
+		segundo = primeiro;
+		primeiro = 10;
+		
+		System.out.println(segundo);
+	}
+}
